96101dc075de2a7e535d597538e849ba929f8761
anilmhrzn <anilmaharjan995@gmail.com>
2023-06-10 11:11:09 +0545

    completed practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-//Todo: make whole file from scratch and make a container and make use of box decoration to use Linear gradient make use of begin and end  enc
 import 'package:flutter/material.dart';
-import 'gradient_container_with_text.dart';
-void main() {
+import 'package:my_first/button_container.dart';
+void main(){
   runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.yellow,
-      // floatingActionButton: ,
-
-      body: MyGradientContainerWithText(),
-
-    ),
-  ));
+  home: Scaffold(
+    body: ButtonContainer(),
+  ),
+  ),
+  );
 }
-
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-class StyledText extends StatelessWidget{
-  const StyledText({super.key});
-  @override
-  Widget build(context){
-     return const Text(
-      'data',
-      style: TextStyle(fontSize: 28, color: Colors.blueAccent),
-    );
-  }
 
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text(
+      "hell",
+      style: TextStyle(
+        fontSize: 40,
+        color: Colors.red,
+        // backgroundColor: Colors.blue
+      ),
+    );
+
+  }
 }
